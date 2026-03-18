ef50b5fcd5b90aefdc2abfb12681ce146ba9c605
Lyle Kuykendall <lyle.kuykendall@gmail.com>
2026-03-17 19:51:13 -0600

    update main.go to use validation functions, also implement some basic logging

--- cmd/main.go
+++ cmd/main.go
@@ -1,42 +1,61 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log/slog"
 	"os"
-	"strconv"
 
 	dispatcher "smarter-screen"
 )
 
+func run(args []string) error {
+	if len(args) != 4 {
+		return errors.New("usage: sort <width> <height> <length> <mass>")
+	}
+
+	width, err := parseArg(args[0], "width")
+	if err != nil {
+		return err
+	}
+
+	height, err := parseArg(args[1], "height")
+	if err != nil {
+		return err
+	}
+
+	length, err := parseArg(args[2], "length")
+	if err != nil {
+		return err
+	}
+
+	mass, err := parseArg(args[3], "mass")
+	if err != nil {
+		return err
+	}
+
+	result := dispatcher.Sort(width, height, length, mass)
+
+	slog.Info("package sorted",
+		"width", width,
+		"height", height,
+		"length", length,
+		"mass", mass,
+		"stack", result,
+	)
+
+	fmt.Println(result)
+	return nil
+}
+
 func main() {
-	if len(os.Args) != 5 {
-		fmt.Fprintf(os.Stderr, "Usage: sort <width> <height> <length> <mass>\n")
+	if err := run(os.Args[1:]); err != nil {
+		var ve *ValidationError
+		if errors.As(err, &ve) {
+			slog.Error("validation failed", "field", ve.Field, "value", ve.Value, "reason", ve.Message)
+		} else {
+			slog.Error(err.Error())
+		}
 		os.Exit(1)
 	}
-
-	width, err := strconv.ParseFloat(os.Args[1], 64)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Invalid width: %s\n", os.Args[1])
-		os.Exit(1)
-	}
-
-	height, err := strconv.ParseFloat(os.Args[2], 64)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Invalid height: %s\n", os.Args[2])
-		os.Exit(1)
-	}
-
-	length, err := strconv.ParseFloat(os.Args[3], 64)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Invalid length: %s\n", os.Args[3])
-		os.Exit(1)
-	}
-
-	mass, err := strconv.ParseFloat(os.Args[4], 64)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Invalid mass: %s\n", os.Args[4])
-		os.Exit(1)
-	}
-
-	fmt.Println(dispatcher.Sort(width, height, length, mass))
 }
